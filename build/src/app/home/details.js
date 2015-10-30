b3758ddfcd66b6d98b677ba33d0d9a0867e985fd
schitupolu <schitupolu@FGT-schitupolu.fortinet-us.com>
2015-10-29 23:12:28 -0700

    adding files under bin and build folder.

--- build/src/app/home/details.js
+++
@@ -1,7 +0,0 @@
-angular.module('project.home.details', [])
-
-    .controller('DetailsCtrl', ['$rootScope', '$scope', '$state', 'HomeService', 'P_ConstantsService', 'P_UtilsService',
-        function ($rootScope, $scope, $state, HomeService, P_ConstantsService, P_UtilsService) {
-            console.log("Invoked DetailsCtrl !!!");
-        }]);
-
