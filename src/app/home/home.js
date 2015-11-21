3bb9b363757ef6ac8eaf892e44fc45a1f379522b
schitupolu <schitupolu@FGT-schitupolu.fortinet-us.com>
2015-11-20 19:33:07 -0800

    Changed 'weburl' to 'iframe' instead of 'object'.

--- src/app/home/home.js
+++ src/app/home/home.js
@@ -146,7 +146,7 @@ angular.module('project.home', [
 
             $scope.getCarouselView = function (index, item) {
                 angular.element("#carousel_" + index).html('');
-                angular.element("#carousel_" + index).append('<object class="iframe-class" data="http://' + item.webUrl + '" />');
+                angular.element("#carousel_" + index).append('<iframe class="iframe-class" frameborder="0" scrolling=no src="http://' + item.webUrl + '" ></iframe>');
             };
         }]);
 
